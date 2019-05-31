4bdc4f41ae1c5a8e7025a1e6d29664b4ba6d3ada
Daniel Gurney <dgurney99@gmail.com>
2019-05-31 20:31:48 +0300

    No need for shorthand here

--- error.go
+++ error.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-func (t trends) catch(err error) string {
+func (trends) catch(err error) string {
 	var e []Trend
 	for i := 0; i < 6; i++ {
 		e = append(e, Trend{"https://search.itunes.apple.com/WebObjects/MZStore.woa/wa/search?src=trending&term=Halsey", fmt.Sprintf("%s", err)})
@@ -18,7 +18,7 @@ func (t trends) catch(err error) string {
 	return eb.String()
 }
 
-func (h hints) catch(err error) string {
+func (hints) catch(err error) string {
 	var e []Hint
 	for i := 0; i < 6; i++ {
 		e = append(e, Hint{fmt.Sprintf("%s", err), fmt.Sprintf("%s", err), "1"})
